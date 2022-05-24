37189dcf6598d59da36054cf23fed8222e08a596
Jake Creely <55953689+jakecreely@users.noreply.github.com>
2022-05-24 17:35:32 +0100

    Learning SASS and implementing design spec

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'react-dom';
+
+function NavBar() {
+    return(
+        <div>
+            <nav>
+                <a><h1>Test</h1>Test</a>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
