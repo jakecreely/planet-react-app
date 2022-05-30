7a6b89c79c7a9b4055896f260ed9689a33dd190c
Jake Creely <55953689+jakecreely@users.noreply.github.com>
2022-05-30 20:39:15 +0100

    adjusting hovering on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,44 @@ import 'react-dom';
 
 function NavBar() {
     return(
-        <div id='navbar'>
-                <ul>
-                    <li>
-                        <div id='nav-title'>THE PLANETS</div>
+        <div className='nav-container'>
+                <ul className='navbar'>
+                    <li className='nav-title'>
+                        <div>THE PLANETS</div>
                     </li>
+
                     <li>
-                    <a>Test</a>
+                    <a id='mercury-link'>Mercury</a>
                     </li>
+
                     <li>
-                        <a>Hello</a>
+                    <a id='venus-link'>Venus</a>
                     </li>
+
+                    <li>
+                    <a id='earth-link'>Earth</a>
+                    </li>
+
+                    <li>
+                    <a id='mars-link'>Mars</a>
+                    </li>
+
+                    <li>
+                    <a id='jupiter-link'>Jupiter</a>
+                    </li>
+
+                    <li>
+                    <a id='saturn-link'>Saturn</a>
+                    </li>
+
+                    <li>
+                    <a id='uranus-link'>Uranus</a>
+                    </li>
+
+                    <li>
+                    <a id='neptune-link'>Neptune</a>
+                    </li>
+
                 </ul>
         </div>
     )
