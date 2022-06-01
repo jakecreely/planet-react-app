13db7f34bc4c89eee6e436c9114de94b81f8b51f
Jake Creely <55953689+jakecreely@users.noreply.github.com>
2022-06-01 11:49:48 +0100

    extracting data from json file and assets to public

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import './styles/style.css';
 import './components/NavBar'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'react-dom';
 import NavBar from './components/NavBar';
 import MainContainer from './components/MainContainer';
 
 function App() {
+
+  const [planetData, setPlanetData] = useState(null);
+
+  useEffect(() => {
+    fetch('data.json')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      setPlanetData(data)
+    })
+  }, [])
+
+  const extractPlanet = (planetName) => {
+    data = planetData.filter(item => {
+      return item.name = planetName
+    })
+    console.log(data)
+    return data
+  }
+
+// Set the data from json
+// Create function which finds specified planet
+// Pass that to the navbar
+// Navbar calls function depending on chosen planet
+// This data is then passed to the main component
+
   return (
     <div className="app">
-      <NavBar />
+      <NavBar /> 
       <MainContainer />
     </div>
   );
