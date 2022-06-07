86342fbe4379b790f8ca50071b64e9a4a8b9332a
Jake Creely <55953689+jakecreely@users.noreply.github.com>
2022-06-07 18:44:02 +0100

    Styling the tablet layout

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -4,14 +4,18 @@ import 'react-dom';
 function SidePanel (props) {
 
     return (<div className='info-panel'>
+        <div className='content-container'>
         <h1 className='title'>{props.name.toUpperCase()}</h1>
         <body className='content'>
             <div>{props.data.content}</div>
-            <div>Source: <a href={props.data.source}>Wikipedia <img src='assets/icon-source.svg'></img></a></div>
+            <div>Source: <a className='links' style={{marginLeft: 0}} href={props.data.source}>Wikipedia <img src='assets/icon-source.svg'></img></a></div>
         </body>
-        <button onClick={() => props.updateLayer("overview")}>Overview</button>
-        <button onClick={() => props.updateLayer("structure")}>Structure</button>
-        <button onClick={() => props.updateLayer("geology")}>Geology</button>
+        </div>
+        <div className='button-container'>
+        <button className='buttons' onClick={() => props.updateLayer("overview")}>Overview</button>
+        <button className='buttons' onClick={() => props.updateLayer("structure")}>Structure</button>
+        <button className='buttons' onClick={() => props.updateLayer("geology")}>Geology</button>
+        </div>
     </div>)
 }
 
